90d0f8b61c1ff7cc33bd92a841fb036f1770354d
jozefvlcek <xvlcekj@stuba.sk>
2024-04-22 11:33:08 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -32,6 +32,10 @@ switch ($command) {
    "mongo" {
     mongo up
    }
+
+   "docker" {
+       docker build -t jozefvlcekbp/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+   }
     default {
         throw "Unknown command: $command"
     }
